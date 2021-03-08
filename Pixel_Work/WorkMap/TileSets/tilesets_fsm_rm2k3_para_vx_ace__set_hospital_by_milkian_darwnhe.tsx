42ea27674b4eb4d709238603e37368fa61fc25c3
Jean-Denis Koeck <jdkoeck@gmail.com>
2021-03-08 14:00:04 +0100

    Add tilesets

---
+++ Pixel_Work/WorkMap/TileSets/tilesets_fsm_rm2k3_para_vx_ace__set_hospital_by_milkian_darwnhe.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tilesets_fsm_rm2k3_para_vx_ace__set_hospital_by_milkian_darwnhe" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../TileSets pour bureau/tilesets_fsm_rm2k3_para_vx_ace__set_hospital_by_milkian_darwnhe.png" width="512" height="512"/>
+</tileset>
