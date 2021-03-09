eda2abb918972dd6216b5367b61fbca009e860c4
jeandenis-k <jdkoeck@gmail.com>
2021-03-09 13:47:00 +0000

    Deploying to gh-pages from master @ d7b3ff2a1121ad6a780a2c8d1cdd3d7b1fe25ac5 🚀

---
+++ TileSets/pokemon_tileset_from_public_tiles_32x32_by_chaoticcherrycake_dab2byf.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="pokemon_tileset_from_public_tiles_32x32_by_chaoticcherrycake_dab2byf" tilewidth="32" tileheight="32" tilecount="5208" columns="8">
+ <image source="../tile_images/pokemon_tileset_from_public_tiles_32x32_by_chaoticcherrycake_dab2byf.png" trans="ff00ff" width="256" height="20832"/>
+ <terraintypes>
+  <terrain name="earth_way" tile="2104"/>
+ </terraintypes>
+ <tile id="2104" terrain=",,,0"/>
+ <tile id="2105" terrain=",,0,0"/>
+ <tile id="2106" terrain=",,0,"/>
+ <tile id="2112" terrain=",0,,0"/>
+ <tile id="2113" terrain="0,0,0,0"/>
+ <tile id="2114" terrain="0,,0,"/>
+ <tile id="2120" terrain=",0,,"/>
+ <tile id="2121" terrain="0,0,,"/>
+ <tile id="2122" terrain="0,,,"/>
+</tileset>
